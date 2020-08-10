c31e1405d6546a468b25cd92065c9e293290646b
zer0grand <45867513+zer0grand@users.noreply.github.com>
2020-08-09 17:57:06 -0700

    Update DrawLineObjects.cs

--- DrawLineObjects.cs
+++ DrawLineObjects.cs
@@ -5,39 +5,39 @@ using System.IO;
 using SimpleJSON;
 using System;
 
-// imports drawing line into unity
 public class DrawLineObjects : MonoBehaviour
 {
     public GameObject linePrefab;
-    public bool loop;
+    public string fileName;
+    public float overrideWidth = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
         var importedJson = JSON.Parse(ReadString());
         for (int i=0; i < importedJson["groups"].Count; i++) {
-            LineRenderer lr = Instantiate(linePrefab).GetComponent<LineRenderer>();
-            int pointCount = importedJson["groups"][i].Count + Convert.ToInt32(loop);
-            Vector3[] pointList = new Vector3[pointCount];
-            for (int j=0; j < pointCount - Convert.ToInt32(loop); j++) {
-                pointList[j] = importedJson["groups"][i][j].ReadVector3();
+            LineRenderer lr = Instantiate(linePrefab, transform).GetComponent<LineRenderer>();
+            if (importedJson["groups"][i]["loop"]) {
+                lr.loop = true;
             }
-            if (loop) {
-                pointList[pointCount-1] = importedJson["groups"][i][0].ReadVector3();
+            if (overrideWidth > 0) {
+                lr.widthCurve = AnimationCurve.Linear(0, overrideWidth, 1, overrideWidth);
+            } else {
+                lr.widthCurve = AnimationCurve.Linear(0, importedJson["groups"][i]["width"], 1, importedJson["groups"][i]["width"]);
+            }
+            int pointCount = importedJson["groups"][i]["points"].Count;
+            Vector3[] pointList = new Vector3[pointCount];
+            for (int j=0; j < pointCount; j++) {
+                pointList[j] = importedJson["groups"][i]["points"][j].ReadVector3();
             }
             lr.positionCount = pointList.Length;
             lr.SetPositions(pointList);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     string ReadString()
     {
-        string path = "Assets/Scripts/CylJson.json";
+        string path = "Assets/LineObjects/"+ fileName +".json";
 
         //Read the text from directly from the test.txt file
         StreamReader reader = new StreamReader(path); 
